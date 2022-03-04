f449cc72dc648a44b6d2ecfe8520d62d748d251e
Harimad <leesh68481@gmail.com>
2022-03-04 18:46:56 +0900

    React 기본 강좌 2-5. 웹팩으로 빌드하기

--- lecture/(2)끝말잇기/WordRelay.jsx
+++ lecture/(2)끝말잇기/WordRelay.jsx
@@ -2,9 +2,17 @@ const React = require('react')
 const { Component } = React
 
 class WordRelay extends Component {
-  state = {}
+  state = {
+    text: 'Hello, 친구들 빡빡이 아조씨야',
+  }
 
-  render() {}
+  render() {
+    return (
+      <>
+        <h1>{this.state.text}</h1>
+      </>
+    )
+  }
 }
 
 module.exports = WordRelay
--- lecture/(2)끝말잇기/webpack.config.js
+++ lecture/(2)끝말잇기/webpack.config.js
@@ -12,8 +12,22 @@ module.exports = {
   // entry랑 output이 가장 중요
   entry: {
     // 입력
-    app: ['./WordRelay.jsx'],
+    app: ['./client.jsx'],
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, //js나 jsx파일은 아래 옛날 브라우저에도 돌아가게 하겠다
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-proposal-class-properties'],
+        },
+      },
+    ],
+  },
+
   output: {
     // 출력
     path: path.join(__dirname, 'dist'), //C:\\users\leesh\webstorm\react-webgame\lecture\dist
