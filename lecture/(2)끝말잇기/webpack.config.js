caff0f1f5d92bf8d80bb7f6f1b3bbb23e5fb3546
Harimad <leesh68481@gmail.com>
2022-03-04 17:16:55 +0900

    React 기본 강좌 2-4. 모듈 시스템과 웹팩 설정

--- lecture/(2)끝말잇기/webpack.config.js
+++ lecture/(2)끝말잇기/webpack.config.js
@@ -1,1 +1,22 @@
-module.exports = {}
+const path = require('path')
+
+module.exports = {
+  name: 'word-relay-setting',
+  mode: 'development', //실서비스: production
+  devtool: 'eval', //빠르게 하겠다
+  resolve: {
+    //파일확장자를 저장 후 찾아줌
+    extensions: ['.js', '.jsx'],
+  },
+
+  // entry랑 output이 가장 중요
+  entry: {
+    // 입력
+    app: ['./WordRelay.jsx'],
+  },
+  output: {
+    // 출력
+    path: path.join(__dirname, 'dist'), //C:\\users\leesh\webstorm\react-webgame\lecture\dist
+    filename: 'app.js',
+  },
+}
