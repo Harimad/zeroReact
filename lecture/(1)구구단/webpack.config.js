d1f1992a286027a58a3fefd960791f87cafeffbc
Harimad <leesh68481@gmail.com>
2022-03-07 18:19:37 +0900

    2.7 보완

--- lecture/(1)구구단/webpack.config.js
+++ lecture/(1)구구단/webpack.config.js
@@ -4,13 +4,13 @@ const webpack = require('webpack')
 module.exports = {
   name: 'gugudan-setting',
   mode: 'development',
-  devtool: 'eval', //개발끝나면 hidden-source-map로 바꾸기
+  devtool: 'inline-source-map', //개발끝나면 hidden-source-map로 바꾸기
   resolve: {
     extensions: ['.js', '.jsx'],
   },
 
   entry: {
-    app: ['./client.jsx'],
+    app: './client',
   },
 
   module: {
@@ -23,10 +23,7 @@ module.exports = {
             [
               '@babel/preset-env', //자동으로 옛날 브라우저 지원해주는 기능
               {
-                targets: {
-                  // browsers: ['> 5% in KR', 'last 2 chrome versions'], //크롬 버전 최근 2개 까지 지원
-                  browsers: ['> 1% in KR'],
-                },
+                targets: { browsers: ['> 1% in KR'] }, //browserlist
                 debug: true,
               },
             ],
@@ -37,8 +34,7 @@ module.exports = {
       },
     ],
   },
-  // 확장프로그램
-  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
+  plugins: [], // 확장프로그램
 
   output: {
     path: path.join(__dirname, 'dist'),
