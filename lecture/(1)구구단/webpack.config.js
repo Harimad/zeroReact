fe4dab8dc6e0076c4e89e0194cd940b5e9ab5093
Harimad <leesh68481@gmail.com>
2022-03-07 18:04:40 +0900

    React 기본 강좌 2-7. @babel/preset-env와 plugins~

--- lecture/(1)구구단/webpack.config.js
+++ lecture/(1)구구단/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   name: 'gugudan-setting',
@@ -18,11 +19,26 @@ module.exports = {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: [
+            [
+              '@babel/preset-env', //자동으로 옛날 브라우저 지원해주는 기능
+              {
+                targets: {
+                  // browsers: ['> 5% in KR', 'last 2 chrome versions'], //크롬 버전 최근 2개 까지 지원
+                  browsers: ['> 1% in KR'],
+                },
+                debug: true,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: [],
         },
       },
     ],
   },
+  // 확장프로그램
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
 
   output: {
     path: path.join(__dirname, 'dist'),
