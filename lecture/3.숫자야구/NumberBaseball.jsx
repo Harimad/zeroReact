1e61c2f3bcf747469ca5c23c94edc24b04586064
Harimad <leesh68481@gmail.com>
2022-03-25 16:57:57 +0900

    3-3. 리액트 반복문(key)

--- lecture/3.숫자야구/NumberBaseball.jsx
+++ lecture/3.숫자야구/NumberBaseball.jsx
@@ -28,10 +28,30 @@ class NumberBaseball extends Component {
 				<div>시도: {this.state.tries.length}</div>
 				<ul>
 					{
-						['1', '2', '3', '4', '5'].map((item) => {
-							return <li>{item}</li>
+						[
+							['배열Apple', 'Good'],
+							['배열Banana', 'Bad'],
+							['배열Tangerine', 'sour'],
+							['배열Grape', 'bitter']
+						].map((item, i) => {
+							return <li key={item[0]+item[1]}>{item[0]} - {item[1]} -{i}</li>
 						})
 					}
+					{
+						[
+							{fruit: '객체Apple', taste: 'Good'},
+							{fruit: '객체Banana', taste: 'Bad'},
+							{fruit: '객체Tangerine', taste: 'sour'},
+							{fruit: '객체Grape', taste: 'bitter'},
+						].map((item, i) => {
+							return <li key={item.fruit+item.taste}>{item.fruit}-{item.taste}-{i}</li>
+						})
+					}
+
+					<li><b>일반Apple</b> - Good</li>
+					<li><b>일반Banana</b> - Bad</li>
+					<li><b>일반Tangerine</b> - sour</li>
+					<li><b>일반Grape</b> - bitter</li>
 				</ul>
 			</>
 		)
