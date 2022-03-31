ef39a236fd5d84fbda7914043c6779c440e0acfb
Harimad <leesh68481@gmail.com>
2022-03-31 22:16:16 +0900

    4-2. setTimeout 넣어 반응속도체크

--- lecture/4.반응속도체크/ResponseCheck.jsx
+++ lecture/4.반응속도체크/ResponseCheck.jsx
@@ -6,8 +6,41 @@ class ResponseCheck extends Component {
     message: '클릭해서 시작하세요',
     result: [],
   }
+  timeout
+  startTime
+  endTime
 
-  onClickScreen = () => {}
+  onClickScreen = () => {
+    const { state, message, result } = this.state
+    if (state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요',
+      })
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: '지금 클릭',
+        })
+        this.startTime = new Date()
+      }, Math.random() * 1000 + 2000)
+    } else if (state === 'ready') {
+      clearTimeout(this.timeout)
+      this.setState({
+        state: 'waiting',
+        message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요',
+      })
+    } else if (state === 'now') {
+      this.endTime = new Date()
+      this.setState(prevState => {
+        return {
+          state: 'waiting',
+          message: '클릭해서 시작하세요',
+          result: [...prevState.result, this.endTime - this.startTime],
+        }
+      })
+    }
+  }
 
   onReset = () => {
     this.setState({
