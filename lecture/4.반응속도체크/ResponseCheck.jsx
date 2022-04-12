0adaa660bcdfe3523c2b99f7a4554e576b393599
Harimad <leesh68481@gmail.com>
2022-04-12 14:57:04 +0900

    4-4. 반응속도체크 Hooks로 전환하기

--- lecture/4.반응속도체크/ResponseCheck.jsx
+++ lecture/4.반응속도체크/ResponseCheck.jsx
@@ -1,74 +1,58 @@
-import React, { Component } from 'react'
+import React, { useState, useRef } from 'react'
 
-class ResponseCheck extends Component {
-  state = {
-    state: 'waiting',
-    message: '클릭해서 시작하세요',
-    result: [],
-  }
-  timeout
-  startTime
-  endTime
+const ResponseCheck = () => {
+  const [state, setState] = useState('waiting')
+  const [message, setMessage] = useState('클릭해서 시작하세요')
+  const [result, setResult] = useState([])
+  const timeout = useRef(null)
+  const startTime = useRef(0)
+  const endTime = useRef(0)
 
-  onClickScreen = () => {
-    const { state, message, result } = this.state
+  const onClickScreen = () => {
     if (state === 'waiting') {
-      this.setState({
-        state: 'ready',
-        message: '초록색이 되면 클릭하세요',
-      })
-      this.timeout = setTimeout(() => {
-        this.setState({
-          state: 'now',
-          message: '지금 클릭',
-        })
-        this.startTime = new Date()
+      timeout.current = setTimeout(() => {
+        setState('now')
+        setMessage('지금 클릭')
+        startTime.current = new Date()
       }, Math.random() * 1000 + 2000)
+      setState('ready')
+      setMessage('초록색이 되면 클릭하세요')
     } else if (state === 'ready') {
-      clearTimeout(this.timeout)
-      this.setState({
-        state: 'waiting',
-        message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요',
-      })
+      clearTimeout(timeout)
+      setState('waiting')
+      setMessage('너무 성급하시군요! 초록색이 된 후에 클릭하세요.')
     } else if (state === 'now') {
-      this.endTime = new Date()
-      this.setState(prevState => {
-        return {
-          state: 'waiting',
-          message: '클릭해서 시작하세요',
-          result: [...prevState.result, this.endTime - this.startTime],
-        }
-      })
+      endTime.current = new Date()
+      setState('waiting')
+      setMessage('클릭해서 시작하세요')
+      setResult(prevState => [
+        ...prevState,
+        endTime.current - startTime.current,
+      ])
     }
   }
 
-  onReset = () => {
-    this.setState({
-      result: [],
-    })
+  const onReset = () => {
+    setResult([])
   }
 
-  renderAverage = () => {
-    const { result } = this.state
+  const renderAverage = () => {
     return result.length === 0 ? null : (
       <>
-        <div>평균 시간 {result.reduce((a, c) => a + c) / result.length}</div>
-        <button onClick={this.onReset}>리셋</button>
+        <div>{result.reduce((a, b) => a + b / result.length)}</div>
+        <button onClick={onReset}>Reset</button>
       </>
     )
   }
 
-  render() {
-    const { state, message } = this.state
-    return (
-      <>
-        <div id="screen" className={state} onClick={this.onClickScreen}>
-          {message}
-        </div>
-        {this.renderAverage()}
-      </>
-    )
-  }
+  return (
+    <>
+      <div id="screen" className={state} onClick={onClickScreen}>
+        {message}
+      </div>
+      {renderAverage()}
+    </>
+  )
 }
 
 export default ResponseCheck
--- lecture/4.반응속도체크/client.jsx
+++ lecture/4.반응속도체크/client.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// Before
+// import ReactDOM from 'react-dom'
+// ReactDOM.render(<ResponseCheck />, document.querySelector('#root'))
 
 import ResponseCheck from './ResponseCheck.jsx'
 
-ReactDOM.render(<ResponseCheck />, document.querySelector('#root'))
+//After
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<ResponseCheck />)
