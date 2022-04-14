221148953db8f05f352d5fe5ad0a9e51d428ed92
Harimad <leesh68481@gmail.com>
2022-04-14 16:25:45 +0900

    7-1. 틱택토와 useRedcuer 소개

--- lecture/7.틱택토/TicTacToe.jsx
+++ lecture/7.틱택토/TicTacToe.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import Table from './Table'
 
+const initialState = {
+  winner: '',
+  turn: '0',
+  tableData: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ],
+}
+
+const reducer = (state, action) => {}
+
 const TicTacToe = () => {
-  return <></>
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  // const [winner, setWinner] = useState('')
+  // const [turn, setTurn] = useState('0')
+  // const [tableData, setTableData] = useState([
+  //   ['', '', ''],
+  //   ['', '', ''],
+  //   ['', '', ''],
+  // ])
+
+  return (
+    <>
+      <Table />
+      {winner && <div>{winner}님의 승리</div>}
+    </>
+  )
 }
 export default TicTacToe
